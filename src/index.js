35ca2405c5289165096393015a3009e93b61374a
OussamaAlouat <al317257@uji.es>
2019-02-14 07:36:33 +0100

    added End-point '/' to check thah server runs well

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors'
 import db from './db';
 import configuration from './config';
+import routes from './routes';
 
 //Init the app server
 const app = express();
@@ -13,7 +14,7 @@ const app = express();
 app.use(bodyParser());
 app.use(cors());
 
-
+app.use('/', routes())
 //CONFIG ---------------
 const config = configuration(app);
 
