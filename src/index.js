8cee921b2f9e7e959d44cb39ae969d4868a7ffcb
OussamaAlouat <al317257@uji.es>
2019-02-14 07:30:07 +0100

    Database configuration

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors'
+import db from './db';
+import configuration from './config';
 
 //Init the app server
 const app = express();
 
-//MIDDLEWARES
+
+
+//MIDLEWARES
 app.use(bodyParser());
 app.use(cors());
 
 
-app.listen(process.env.PORT || 3000, () => {
-    const listeningPort = process.env.PORT || 3000;
+//CONFIG ---------------
+const config = configuration(app);
+
+//DATABASE CONNECTION
+db.connect(config);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    const err = Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// catch 500 internal sever error
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json(err);
+});
+
+app.listen(process.env.PORT || config.port, () => {
+    const listeningPort = process.env.PORT || config.port;
     console.log('Server listening on port ' + listeningPort);
 });
 
